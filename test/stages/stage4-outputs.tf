fb73025f4e6920fdf5ccbf5670d5285b6d0bb88e
Sasikanth G <bgumpana@in.ibm.com>
2022-03-09 15:02:39 +0530

    Data Fabric Setup on CP4D
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage4-outputs.tf
+++ test/stages/stage4-outputs.tf
@@ -5,18 +5,14 @@ resource null_resource write_outputs {
 
     environment = {
       OUTPUT = jsonencode({
-        name        = module.cp-data-virtualization.name
-        inst_name   = module.cp-data-virtualization.inst_name
-        sub_chart   = module.cp-data-virtualization.sub_chart
-        sub_name   = module.cp-data-virtualization.sub_name
-        operator_namespace = module.cp-data-virtualization.operator_namespace
-        cpd_namespace= module.cp-data-virtualization.cpd_namespace
-        branch      = module.cp-data-virtualization.branch
-        namespace   = module.cp-data-virtualization.namespace
-        server_name = module.cp-data-virtualization.server_name
-        layer       = module.cp-data-virtualization.layer
-        layer_dir   = module.cp-data-virtualization.layer == "infrastructure" ? "1-infrastructure" : (module.cp-data-virtualization.layer == "services" ? "2-services" : "3-applications")
-        type        = module.cp-data-virtualization.type
+        name        = module.cp-data-fabric.name
+        cpd_namespace= module.cp-data-fabric.cpd_namespace
+        branch      = module.cp-data-fabric.branch
+        namespace   = module.cp-data-fabric.namespace
+        server_name = module.cp-data-fabric.server_name
+        layer       = module.cp-data-fabric.layer
+        layer_dir   = module.cp-data-fabric.layer == "infrastructure" ? "1-infrastructure" : (module.cp-data-virtualization.layer == "services" ? "2-services" : "3-applications")
+        type        = module.cp-data-fabric.type
       })
     }
   }
