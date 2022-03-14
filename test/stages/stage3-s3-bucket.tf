684aab3cbc3a435593975dc9437e0ad7a3bf6c6e
Sasikanth G <bgumpana@in.ibm.com>
2022-03-14 14:37:10 +0530

    Data Fabric on CP4D
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage3-s3-bucket.tf
+++ test/stages/stage3-s3-bucket.tf
@@ -1,4 +1,4 @@
-module "aws-s3-bucket" {
-  source = "github.com/cloud-native-toolkit/terraform-aws-s3-bucket.git"
-  bucket_id = module.aws_s3_instance.s3_bucket_id
-}
+# module "aws-s3-bucket" {
+#   source = "github.com/cloud-native-toolkit/terraform-aws-s3-bucket.git"
+#   bucket_id = module.aws_s3_instance.s3_bucket_id
+# }
