89c8dee37e246889e8f1c943832f793f8a42eee0
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-06-28 12:28:28 +0530

    Workflow update
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -17,6 +17,8 @@ SERVER_NAME=$(jq -r '.server_name // "default"' gitops-output.json)
 LAYER=$(jq -r '.layer_dir // "2-services"' gitops-output.json)
 TYPE=$(jq -r '.type // "base"' gitops-output.json)
 
+sleep 600
+
 mkdir -p .testrepo
 
 git clone https://${GIT_TOKEN}@${GIT_REPO} .testrepo
