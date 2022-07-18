25c50eb543355971affdfef848f0f11c6069ed06
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-07-18 16:31:34 +0530

    Workflow update and Services Check
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- test/stages/stage2-cp-wkc.tf
+++ test/stages/stage2-cp-wkc.tf
@@ -1,19 +1,19 @@
-module "gitops_cp_wkc" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-cp-wkc"
+# module "gitops_cp_wkc" {
+#   source = "github.com/cloud-native-toolkit/terraform-gitops-cp-wkc"
 
 
-  gitops_config = module.gitops.gitops_config
-  git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  namespace = module.gitops_namespace.name
-  kubeseal_cert = module.gitops.sealed_secrets_cert
-  #operator_namespace = module.gitops_cp4d_operator.namespace
-  #cpd_namespace = module.gitops_cp4d_instance.namespace
-  operator_namespace = "cpd-operators"
-  cpd_namespace = "cp4d"
-  storage_vendor = "ocs"
-  storage_class = "ocs-storagecluster-cephfs"
-  instance_version = "4.0.9"
-  sub_syncwave = "-20"
-  inst_syncwave = "-18"
-}
+#   gitops_config = module.gitops.gitops_config
+#   git_credentials = module.gitops.git_credentials
+#   server_name = module.gitops.server_name
+#   namespace = module.gitops_namespace.name
+#   kubeseal_cert = module.gitops.sealed_secrets_cert
+#   #operator_namespace = module.gitops_cp4d_operator.namespace
+#   #cpd_namespace = module.gitops_cp4d_instance.namespace
+#   operator_namespace = "cpd-operators"
+#   cpd_namespace = "cp4d"
+#   storage_vendor = "ocs"
+#   storage_class = "ocs-storagecluster-cephfs"
+#   instance_version = "4.0.9"
+#   sub_syncwave = "-20"
+#   inst_syncwave = "-18"
+# }
